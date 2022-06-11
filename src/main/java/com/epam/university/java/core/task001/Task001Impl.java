eb9ae2f0f9abf0b5c8c97eb87fcff89e4cb0df6a
Roman Smaliuk <roman.smaliuk@gmail.com>
2022-06-11 17:11:14 +0300

    Task 001 complete

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -3,21 +3,53 @@ package com.epam.university.java.core.task001;
 public class Task001Impl implements Task001 {
     @Override
     public double addition(String firstNumber, String secondNumber) {
-        return 0;
+        try {
+            if (firstNumber != null && secondNumber != null) {
+                return Double.parseDouble(firstNumber.strip()) + Double.parseDouble(secondNumber.strip());
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
     }
 
     @Override
     public double subtraction(String firstNumber, String secondNumber) {
-        return 0;
+        try{
+            if (firstNumber != null && secondNumber != null){
+                return Double.parseDouble(firstNumber.strip()) - Double.parseDouble(secondNumber.strip());
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }catch (NumberFormatException e){
+            throw new NumberFormatException();
+        }
     }
 
     @Override
     public double multiplication(String firstNumber, String secondNumber) {
-        return 0;
+        try{
+            if (firstNumber != null && secondNumber != null){
+                return Double.parseDouble(firstNumber.strip()) * Double.parseDouble(secondNumber.strip());
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }catch (NumberFormatException e){
+            throw new NumberFormatException();
+        }
     }
 
     @Override
     public double division(String firstNumber, String secondNumber) {
-        return 0;
+        try{
+            if (firstNumber != null && secondNumber != null){
+                return Double.parseDouble(firstNumber.strip()) / Double.parseDouble(secondNumber.strip());
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }catch (NumberFormatException e){
+            throw new NumberFormatException();
+        }
     }
 }
